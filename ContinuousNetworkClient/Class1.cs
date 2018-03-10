92540243df99f162f8565e0bdfa3b200ff457ca6
boredbone <Boredbone@users.noreply.github.com>
2018-03-10 14:55:03 +0900

    プロジェクト ファイルを追加します。

---
+++ ContinuousNetworkClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuousNetworkClient
+{
+    public class Class1
+    {
+    }
+}
